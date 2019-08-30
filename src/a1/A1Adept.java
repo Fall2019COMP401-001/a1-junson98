96cb6da34b49abb127b0fe8ee5e80c7a20173edf
Jun Son <junson98@gmail.com>
2019-08-30 11:41:32 -0400

    Finished novice and adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,64 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int storeItemnum = scan.nextInt(); 
 
-		// Your code follows here.
+		String[] Itemlist = new String[storeItemnum];
+		double[] price = new double[storeItemnum];
 		
+		for (int i = 0; i < storeItemnum; i++) {
+			Itemlist[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		
+		int numCustomer = scan.nextInt();
+		double total = 0.0;
+		
+		String[] firstName = new String[numCustomer];
+		String[] lastName = new String[numCustomer];
+		double biggest =0;
+		double smallest = 0;
+		double avgTotal = 0;
+		String biggestName = "";
+		String smallestName = "";
+		for (int i = 0; i < numCustomer; i++) {
+			
+			firstName[i] = scan.next();
+			lastName[i] = scan.next();
+			
+			int numItems = scan.nextInt();
+			total = 0;
+			for(int j = 0; j < numItems; j++) {
+				
+				int indNum = scan.nextInt();
+				String temp = scan.next(); 
+				for (int k=0; k < Itemlist.length; k++) {
+				if(temp.equals(Itemlist[k])) {
+					total += indNum * price[k];
+					} 
+				}
+			
+			}
+			if (smallest == 0) {
+				smallest = total;
+			}
+			if (total >= biggest) {
+				biggest = total;
+				biggestName = firstName[i] + " " + lastName[i];
+			} 
+			if (total <= smallest) {
+				smallest = total;
+				smallestName = firstName[i] + " " + lastName[i];
+			}
+			
+			avgTotal += total;
+			
+		}
+		avgTotal = avgTotal / numCustomer;
+		//System.out.println(firstName[i].charAt(0)+ ". " + lastName[i] + ": " + total);
+		System.out.println("Biggest: " + biggestName + " " + "(" +String.format("%.2f", biggest)+")");
+		System.out.println("Smallest: " + smallestName + " " + "(" + smallest + ")");
+		System.out.println("Average: " + String.format("%.2f", avgTotal));
 	}
+		
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,25 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		Scanner scan = new Scanner(System.in); 
+		int numCustomer = scan.nextInt();
+		double total = 0.0;
+		String[] firstName = new String[numCustomer];
+		String[] lastName = new String[numCustomer];
+		for (int i = 0; i < numCustomer; i++) {
+			total = 0;
+			firstName[i] = scan.next();
+			lastName[i] = scan.next();
+			int numItems = scan.nextInt();
+			for(int j = 0; j < numItems; j++) {
+				int indNum = scan.nextInt();
+				String itemName = scan.next();
+				double price = scan.nextDouble();
+				total += indNum * price;
+			}
+			System.out.println(firstName[i].charAt(0)+ ". " + lastName[i] + ": " + String.format("%.2f", total));
+		}
+		
 		
 	}
 }
