b86549e34172a6806dad4a8f76461fc655a7cfec
Jun Son <junson98@gmail.com>
2019-09-02 02:01:56 -0400

    Jedi revised

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -4,48 +4,83 @@ import java.util.Scanner;
 
 public class A1Jedi {
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		int storeItemnum = scan.nextInt(); 
+   public static void main(String[] args) {
+      String itemName;
+      double price = 0;
+      String firstName;
+      String lastName;
+      int itemKind =0;
+      int itemQuant =0;
+      double total =0.0;
+      double average =0.0;
+      double currentTotal= 0;
+   
+      
+      
+      
+      
+      Scanner scan = new Scanner(System.in);
+      int itemNum = scan.nextInt();
+      
+      String[] fruit = new String[itemNum];
+      double[] costs = new double[itemNum];
+      int[] customer = new int[itemNum];
+      int[] it = new int[itemNum];
+      int[] custRepeated = new int[itemNum];
+for (int i =0; i < itemNum; i ++) 
+{
+   itemName = scan.next();
+   price = scan.nextDouble();
+   fruit[i] = itemName;
+   costs[i] = price;
+   customer[i] = 0;
+   it[i] = 0;
+   custRepeated[i] =0;
+   }
+int numOfcustomer = scan.nextInt();
 
-		String[] Itemlist = new String[storeItemnum];
-		double[] price = new double[storeItemnum];
-		
-		for (int i = 0; i < storeItemnum; i++) {
-			Itemlist[i] = scan.next();
-			price[i] = scan.nextDouble();
-		}
-		
-		int numCustomer = scan.nextInt();
-		int[] counterArray = new int[storeItemnum];
-		int[] counterCustomer = new int[storeItemnum];
-		String[] firstName = new String[numCustomer];
-		String[] lastName = new String[numCustomer];
-		for (int i = 0; i < numCustomer; i++) {
-			
-			firstName[i] = scan.next();
-			lastName[i] = scan.next();
-			
-			int numItems = scan.nextInt();
-			
-			for(int j = 0; j < numItems; j++) {
-				int indNum = scan.nextInt();
-				String temp = scan.next(); 
-				for (int a = 0; a < Itemlist.length; a++) {
-					if (temp.equals(Itemlist[a])) {
-						counterArray[a] += indNum;
-						counterCustomer[a]++;
-					}
-				}
-			}
-		}
-		for (int b = 0; b < Itemlist.length; b++) {
-			if (counterArray[b] == 0) {
-				System.out.println("No customers bought " + Itemlist[b]);
-			} else {
-				System.out.println(counterCustomer[b] + " " + "customers bought " + counterArray[b] + " " + Itemlist[b]);
-			}
-		}
-	}
+for (int j =0; j <numOfcustomer; j++ ) {
+   
+   firstName = scan.next();
+   
+   lastName = scan.next();
+   
+   itemKind = scan.nextInt();
+      
+   for (int k =0; k < itemKind; k ++) {
+         
+         
+         itemQuant = scan.nextInt();
+         
+         String purchasedit = scan.next();
+         
+         for (int o =0; o < itemNum; o++) {
+         
+            if (purchasedit.equals(fruit[o])) {
+               it[o] = it[o] + itemQuant;
+               custRepeated[o] ++;
+               if (custRepeated[o] < 2) {
+            	   customer[o]++;
+               }
+               break;
+            	}
+            }
+   }
+   for (int u=0; u<custRepeated.length; u++) {
+      custRepeated[u] = 0;
+   }
+   
+
+   }
+for (int i =0; i < itemNum; i ++) {
+   if (customer[i] == 0) {
+      System.out.println("No customers bought " + fruit[i]);
+   }
+   else {
+      System.out.println(customer[i] + " customers bought " + it[i] + " " + fruit[i]);
+   }
 }
+
+}
+}
+
